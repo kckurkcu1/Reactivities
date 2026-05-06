4a7d33578f2fe20f6ad8e66b7a4b94d75e5efe03
Kerem Can Kürkçü <kerem.kurkcu@hotmail.com>
2026-05-06 16:41:50 +0300

    x

--- client/src/app/layout/Navbar.tsx
+++ client/src/app/layout/Navbar.tsx
@@ -1,17 +1,14 @@
 import { Group } from "@mui/icons-material";
-import { Box, AppBar, Toolbar, Typography, Container, Button } from "@mui/material";
+import { Box, AppBar, Toolbar, Typography, Container, Button, LinearProgress } from "@mui/material";
 import { NavLink } from "react-router";
+import { Observer } from "mobx-react-lite";
+import { useStore } from "../../lib/hooks/useStore";
 
 export default function NavBar() {
+  const { uiStore } = useStore();
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar
-        position="static"
-        sx={{
-          backgroundImage:
-            "linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)",
-        }}
-      >
+      <AppBar position="static" sx={{ backgroundImage: 'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)', position: 'relative' }}>
         <Container maxWidth="xl">
           <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
 
@@ -67,6 +64,22 @@ export default function NavBar() {
                 )}
               </NavLink>
 
+              <NavLink to="/counter" style={{ textDecoration: "none" }}>
+                {({ isActive }) => (
+                  <Button
+                    color="inherit"
+                    sx={{
+                      fontSize: "1.1rem",
+                      textTransform: "uppercase",
+                      fontWeight: 600,
+                      color: isActive ? "yellow" : "#ffffff"
+                    }}
+                  >
+                    Counter
+                  </Button>
+                )}
+              </NavLink>
+
             </Box>
 
             {/* RIGHT */}
@@ -75,6 +88,22 @@ export default function NavBar() {
             </Button>
 
           </Toolbar>
+          <Observer>
+            {() =>
+              uiStore.isLoading ? (
+                <LinearProgress
+                  color="secondary"
+                  sx={{
+                    position: 'absolute',
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    height: 4, // Adjust height if needed
+                  }}
+                />
+              ) : null
+            }
+          </Observer>
         </Container>
       </AppBar>
     </Box>
